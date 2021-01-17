28237d8d40b8073cc19e0fe38d315b332ad14649
yug <59773019+shubhamshukla24@users.noreply.github.com>
2021-01-18 00:00:22 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    float a,b,product;
+    cout<<"Enter two numbers:";
+    cin>>a>>b;
+    product=a*b;
+    cout<<"Product= "<<product;
+    
+
+	return 0;
+}
